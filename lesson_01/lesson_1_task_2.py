f2a27d7bad28bbadc3e7cf51f53738fd25d019c9
unknown <bernastya83@gmail.com>
2025-11-30 16:05:43 +0300

    lesson1

--- lesson_01/lesson_1_task_2.py
+++ lesson_01/lesson_1_task_2.py
@@ -1,3 +1,3 @@
-my_age = "42"
-my_age = "45"
+my_age = 42
+my_age = 45
 print(my_age)
--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,3 @@
-user_name = input("Как вас зовут? ")
-first_name = ("Настя")
-last_name = ("Берёза")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 print(f"Вас зовут: {last_name} {first_name}")

df03a858ce517d8ae90487c1bcfe9d5db1ec6b45
unknown <bernastya83@gmail.com>
2025-11-30 14:04:52 +0300

    lesson1

---
+++ lesson_01/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Настя"
+print(my_name)
---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,4 @@
+user_name = input("Как вас зовут? ")
+first_name = ("Настя")
+last_name = ("Береза")
+print(f"Вас зовут: {last_name} {first_name}")

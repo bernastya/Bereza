2e3d4b9bba697c863e537b9ca95192798d2a3cb5
unknown <bernastya83@gmail.com>
2025-12-07 22:09:24 +0300

    lesson2

---
+++ lesson_02/lesson2_operators.py
@@ -0,0 +1,11 @@
+crit1 = "red"
+crit2 = "lock"
+
+color = input("Color: ")
+feature = input("Feature: ")
+
+if color == crit1 or feature == crit2:
+    print("Buy it!")
+
+else:
+    print("Похожу, еще посмотрю!")

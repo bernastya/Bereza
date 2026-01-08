aef53efb858d6c86d045996bb6a21cbd8ee88736
unknown <bernastya83@gmail.com>
2026-01-08 20:43:19 +0300

    05_lesson

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(
+     service=FirefoxService(GeckoDriverManager().install()))
+
+
+driver.get("https://www.firefox.com/")
+driver.get("https://the-internet.herokuapp.com/inputs")
+
+sleep(5)
+
+search_field = driver.find_element(By.CSS_SELECTOR, 'input')
+search_field.send_keys("Sky")
+
+sleep(5)
+
+search_field.clear()
+search_field.send_keys("Pro")
+
+sleep(5)
+
+driver.quit()
---
+++ 05_lesson/lesson05_task4.py
@@ -0,0 +1,34 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
+
+
+driver.get("https://www.firefox.com/")
+driver.get("https://the-internet.herokuapp.com/login")
+
+sleep(5)
+
+search_field = driver.find_element(By.NAME, 'username')
+search_field.send_keys("tomsmith")
+
+sleep(3)
+
+search_field = driver.find_element(By.NAME, 'password')
+search_field.send_keys("SuperSecretPassword!")
+
+sleep(3)
+
+driver.find_element(By.TAG_NAME, 'i').click()
+
+text = driver.find_element(By.CSS_SELECTOR, "div#flash.flash.success").text
+
+print(text)
+
+sleep(3)
+
+driver.quit()
